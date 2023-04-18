282f9b33ef05f4668e05e5b1c4def7aafdc1db9a
hahaym6164 <60290070+hahaym6164@users.noreply.github.com>
2023-04-18 19:07:43 -0400

    update

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Button.css";
+interface ButtonProps {
+  label: string;
+}
+
+function Button(props: ButtonProps) {
+  return <button>{props.label}</button>;
+}
+
+export default Button;
